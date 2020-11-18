634174510ade92aa963728c530e59576636848bd
Jesr2104 <jjsotoramos@hotmail.com>
2020-11-18 05:20:09 +0000

    Version: 1.0.00

--- app/src/main/java/com/justjump/filmscave/MainActivity.kt
+++ app/src/main/java/com/justjump/filmscave/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.justjump.filmscave
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.justjump.filmscave.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //load the theme
+        setTheme(R.style.Theme_FilmsCave)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
